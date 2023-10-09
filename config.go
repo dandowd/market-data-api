e92300e1d34b515d265fc17884ec6811f3f62892
Dan Dowd <danieldowd89@gmail.com>
2023-10-09 14:05:29 -0600

    adds config

---
+++ config.go
@@ -0,0 +1,17 @@
+package main
+
+import "os"
+
+type Config struct {
+	AlpacaAPIKey    string
+	AlpacaAPISecret string
+	AlpacaBaseURL   string
+}
+
+func NewConfig() *Config {
+	return &Config{
+		AlpacaAPIKey:    os.Getenv("ALPACA_API_KEY"),
+		AlpacaAPISecret: os.Getenv("ALPACA_API_SECRET"),
+		AlpacaBaseURL:   os.Getenv("ALPACA_BASE_URL"),
+	}
+}
