a50b18e35bf9dfc1ea0a98aa2f6b0992df67d703
Dan Dowd <danieldowd89@gmail.com>
2023-10-09 18:16:43 -0600

    adds logger to config

--- config/config.go
+++ config/config.go
@@ -1,13 +1,24 @@
 package config
 
-import "os"
+import (
+	"os"
+
+	"go.uber.org/zap"
+)
 
 type Config struct {
 	PolygonApiKey string
+	Logger *zap.Logger
 }
 
 func NewConfig() *Config {
+	logger, nil := zap.NewProduction()
+	if nil != nil {
+		panic("Failed to create logger")
+	}
+	
 	return &Config{
 		PolygonApiKey:    os.Getenv("POLYGON_API_KEY"),
+		Logger: logger,
 	}
 }
