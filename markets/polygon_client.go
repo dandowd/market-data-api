c5008d719d5ad38b5a2b5f366e4011d39280fedf
Dan Dowd <danieldowd89@gmail.com>
2023-10-09 18:16:51 -0600

    adds logging to polygon client

--- markets/polygon_client.go
+++ markets/polygon_client.go
@@ -6,21 +6,25 @@ import (
 
 	polygon "github.com/polygon-io/client-go/rest"
 	"github.com/polygon-io/client-go/rest/models"
+	"go.uber.org/zap"
 )
 
 type polygonClient struct {
   client *polygon.Client
+	config *config.Config
 }
 
 func NewPolygonClient(config *config.Config) MarketClient {
 	return &polygonClient{
 		client: polygon.New(config.PolygonApiKey),
+		config: config,
 	}
 }
 
 func (c *polygonClient) GetLatest(tickerSymbol string) (*SymbolInfo, error) {
 	resp, err := c.client.GetLastTrade(context.Background(), &models.GetLastTradeParams{ Ticker: tickerSymbol })
 	if err != nil {
+		c.config.Logger.Error("Error getting last trade", zap.Error(err))
 		return nil, err
 	}
 
